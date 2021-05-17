601c54b3bae3d3bf25f776d32dd898c739c4586e
butterfly1703 <84016795+ramya1703@users.noreply.github.com>
2021-05-17 23:28:42 +0530

    Update urls.py

--- MyFirstDjangoProj/urls.py
+++ MyFirstDjangoProj/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('mypolls/',include('mypolls.urls')),
+    path('myfirstapp/',include('myfirstapp.urls')),
     path('admin/', admin.site.urls),
 ]
